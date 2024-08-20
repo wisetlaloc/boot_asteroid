2ea4b781ba8dd9518607293888c7df2da6930386
wisetlaloc <13391342+wisetlaloc@users.noreply.github.com>
2024-08-21 05:33:16 +0900

    Use color name instead of RGB value for drawing
    Replaced the RGB tuple with the color name "white" in the draw method. This change aims to improve code readability and maintain consistency with other color definitions in the codebase.

--- player.py
+++ player.py
@@ -20,7 +20,7 @@ class Player(CircleShape):
         return [a, b, c]
 
     def draw(self, screen):
-        pygame.draw.polygon(screen, (255, 255, 255), self.triangle(), 2)
+        pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
