26ba9b4ce7cf2eaf70d3effc5e6302a1074c69c5
wisetlaloc <13391342+wisetlaloc@users.noreply.github.com>
2024-08-20 22:45:08 +0900

    Add player movement and drawing logic
    Introduced a Player class with movement and rotation capabilities, inheriting from CircleShape. Updated main.py to instantiate and update the player, and constants.py to include player-related constants.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
@@ -7,13 +8,20 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
+        tick = clock.tick(60)
+        player.update(tick / 1000)
+        dt += tick / 1000
 
 
 if __name__ == "__main__":
