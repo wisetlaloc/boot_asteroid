8915d89a8391cfda026d454af972525f3a067c5b
wisetlaloc <13391342+wisetlaloc@users.noreply.github.com>
2024-08-21 05:21:03 +0900

    Refactor main function to utilize sprite groups
    Implemented separate sprite groups for updatable and drawable objects. This improves code readability and organization by clearly separating game logic updates from rendering operations. Also simplified the timing mechanism for smoother updates.

--- main.py
+++ main.py
@@ -4,24 +4,29 @@ from player import Player
 
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
-    clock = pygame.time.Clock()
-    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
+    tick = clock.tick(60) / 1000
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0))
-        player.draw(screen)
+        for obj in updatable:
+            obj.update(tick)
+        screen.fill("black")
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
-        tick = clock.tick(60)
-        player.update(tick / 1000)
-        dt += tick / 1000
+        dt += tick
 
 
 if __name__ == "__main__":
